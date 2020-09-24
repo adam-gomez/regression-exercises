6e8d2a3f71b5770203cfe6eec782871bfb4dbb79
Adam Gomez <adam.daniel.gomez.787@gmail.com>
2020-09-24 16:56:26 -0500

    completed initial function development in wrangle and split_scale

---
+++ explore.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import warnings
+warnings.filterwarnings("ignore")
+
+import env
+import wrangle
+import split_scale
+
--- wrangle.py
+++ wrangle.py
@@ -6,9 +6,26 @@ import os
 import pandas as pd
 
 def get_connection(db, user=user, host=host, password=password):
+    '''
+    This function creates the url needed to connect to the Codeup SQL database
+    using the information stored in env.py
+
+    Parameters - (db, user=user, host=host, password=password)
+    db = name of the database you wish to access
+    '''
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
 def wrangle_telco():
+    '''
+    This function will select customer_id, monthly_charges, tenure, and total_charges from
+    the customer table of the telco_churn database using the get_connection function 
+    (and credentials in env.py). Only customers with a 2 year contract are selected.
+
+    Blank values in 'total_charges' (made up of the character " ") are replaced with 0
+    The 'total_charges' column is converted to numeric data type
+
+    Parameters - None 
+    '''
     sql_query = 'SELECT customer_id, monthly_charges, tenure, total_charges FROM customers WHERE contract_type_id = 3'
     df = pd.read_sql(sql_query, get_connection('telco_churn'))
     df['total_charges'] = df.total_charges.replace(" ", 0)
